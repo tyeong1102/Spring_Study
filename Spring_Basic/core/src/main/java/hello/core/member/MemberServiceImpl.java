f83adb044d472b3028c0a2ee971c182ca13e5c03
Kim Taeyeong <tyeong1102@gmail.com>
2023-06-21 22:18:22 +0900

    컴포넌트 스캔과 의존관게 자동 주입 시작하기

--- Spring_Basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ Spring_Basic/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("memberService2")
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired // ac.getBean(MemberRepository.class)와 유사하게 동작
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- Spring_Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring_Basic/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,9 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     // 인터페이스에만 의존하도록 설계 변경
